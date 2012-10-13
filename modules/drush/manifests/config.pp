41c6e0245fa70e5d2ca91372396215b1f74d9d87
Mike Pirog <mike@kalamuna.com>
2012-10-12 17:23:08 -0700

    make sure drush is installed before we add custom commands

--- modules/drush/manifests/config.pp
+++ modules/drush/manifests/config.pp
@@ -6,10 +6,11 @@
 class drush::config {
     
     file { "/usr/share/drush/commands/kala":
-      ensure => directory,
-      owner  => root,
-      group  => root,
-      mode   => 755,
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      mode    => 755,
+      require => Class["drush::install"],
     }
     
     file { "/usr/share/drush/commands/kala/createvhost.drush.inc":
