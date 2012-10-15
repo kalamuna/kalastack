4980749708ced160a9b64bb7036edfa940762505
Mike Pirog <mike@kalamuna.com>
2012-10-15 14:16:40 -0700

    only git config the username

--- modules/git/manifests/config.pp
+++ modules/git/manifests/config.pp
@@ -9,7 +9,7 @@ class git::config {
     exec { "gitconfigusername":
       path => "/bin:/usr/bin",
       unless => "cat /home/${::kala_user}/.gitconfig",
-      command => "sudo -u ${::kala_user} git config --global user.name \"${::kala_name}\" + sudo -u ${::kala_user} git config --global user.email \"${::kala_user}@${::hostname}\"",
+      command => "sudo -u ${::kala_user} git config --global user.name \"${::kala_name}\"",
       require => [
         Class[git::install]
       ],
