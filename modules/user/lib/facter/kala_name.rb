1bfd3ec472a1e2cc90f4a0e9eb67eb54fd7e8936
Mike Pirog <mike@kalamuna.com>
2012-10-15 14:00:26 -0700

    kala name facter

---
+++ modules/user/lib/facter/kala_name.rb
@@ -0,0 +1,8 @@
+# kala_name.rb
+
+Facter.add("kala_name") do
+  setcode do
+    Facter::Util::Resolution.exec('/bin/grep 1000 /etc/passwd | cut -f5 -d":" | tr -d /',/'')
+  end
+end
+
--- modules/user/lib/facter/kala_user.rb
+++ modules/user/lib/facter/kala_user.rb
@@ -5,3 +5,4 @@ Facter.add("kala_user") do
     Facter::Util::Resolution.exec('/usr/bin/getent passwd | awk -F: \'$3 == 1000 { print $1 }\'')
   end
 end
+
