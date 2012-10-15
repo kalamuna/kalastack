51d13ecadb246949c0d0ddd4f6764bcca6c0c9e1
Mike Pirog <mike@kalamuna.com>
2012-10-15 14:23:48 -0700

    make git config more delcarative

--- modules/git/manifests/config.pp
+++ modules/git/manifests/config.pp
@@ -8,7 +8,7 @@ class git::config {
     # Sets the git username
     exec { "gitconfigusername":
       path => "/bin:/usr/bin",
-      unless => "cat /home/${::kala_user}/.gitconfig",
+      unless => "grep name /home/${::kala_user}/.gitconfig",
       command => "sudo -u ${::kala_user} git config --global user.name \"${::kala_name}\"",
       require => Class[git::install],
     }
@@ -17,8 +17,8 @@ class git::config {
     exec { "gitconfiguseremail":
       path => "/bin:/usr/bin",
       unless => "grep email /home/${::kala_user}/.gitconfig",
-      command => "sudo -u ${::kala_user} git config --global user.email \"${::kala_name}@${::hostname}\"",
-      require => Exec["gitconfigusername"],
+      command => "sudo -u ${::kala_user} git config --global user.email \"${::kala_user}@${::hostname}\"",
+      require => Class[git::install],
     }
     
 }
