990fc533281cfc0771ecbb7af61a832724154195
Mike Pirog <mike@kalamuna.com>
2012-10-15 12:14:17 -0700

    easier fact for testing

--- modules/user/lib/facter/kala_user.rb
+++ modules/user/lib/facter/kala_user.rb
@@ -2,6 +2,6 @@
 
 Facter.add("kala_user") do
   setcode do
-    Facter::Util::Resolution.exec("usr/bin/getent passwd | awk -F: '$3 == 1000 { print $1 }'")
+    Facter::Util::Resolution.exec('/bin/uname -i')
   end
 end
