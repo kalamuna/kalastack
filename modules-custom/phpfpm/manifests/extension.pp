d0da96a61d1b7ea9971623e9e702c9731000f124
Patrick Connolly <patrick@myplanetdigital.com>
2013-02-19 03:32:23 -0500

    Removed pinning phpfpm::extension packages.

--- modules-custom/phpfpm/manifests/extension.pp
+++ modules-custom/phpfpm/manifests/extension.pp
@@ -10,7 +10,7 @@ define phpfpm::extension (
 ){
   
   package { "${name}":
-    ensure => "${version}",
+    ensure => "present",
     require => Class["phpfpm::install"],
     notify => [Class["phpfpm::service"], Class["nginx::service"]],
   }
