7cc20498e94c41b16deda27a08616591e26c3269
Mike Pirog <mike@kalamuna.com>
2013-07-06 22:45:06 -0700

    dirs for vhosts and mysql data

--- modules/mysql/manifests/server/config.pp
+++ modules/mysql/manifests/server/config.pp
@@ -1,14 +1,21 @@
 /**
- * 
+ *
  * Class that governs the creation of the mysql-server configuration
- * 
+ *
  */
 class mysql::server::config {
-	
-	mysql::my { "my.cnf": 
+
+	mysql::my { "my.cnf":
 	  path => "/etc/mysql/my.cnf",
 	  require => Class["mysql::server::install"],
 	  notify => Class["mysql::server::service"],
 	}
+
+  file { "/etc/kalastack/mysql":
+    ensure  => directory,
+    mode    => 777,
+    owner   => $::kalauser,
+    group   => $::kalauser,
+  }
 
 }
--- modules/nginx/manifests/config.pp
+++ modules/nginx/manifests/config.pp
@@ -1,7 +1,7 @@
 /**
  *
  * Configure nginx
- * 
+ *
  */
 class nginx::config {
   nginx::conf { "nginx.conf":
@@ -9,9 +9,17 @@ class nginx::config {
     require => Class["nginx::install"],
     notify  => Class["nginx::service"],
   }
-  
+
   file { "/etc/nginx/fastcgi_params":
     ensure => file,
     source => "puppet:///modules/nginx/fastcgi_params",
+    require => Class["nginx::install"],
+  }
+
+  file { "/etc/kalastack/vhosts":
+    ensure  => directory,
+    mode    => 777,
+    owner   => $::kalauser,
+    group   => $::kalauser,
   }
 }
