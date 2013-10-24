0df1ba17eb3a264171b9688dbc01638ded49f4a9
Mike Pirog <mike@kalamuna.com>
2013-10-24 11:37:41 -0700

    Remove stray terminus ref

--- modules/drush/manifests/refresh.pp
+++ modules/drush/manifests/refresh.pp
@@ -8,7 +8,7 @@ class drush::refresh {
   exec { "drushrefresh":
     path    => "/bin:/usr/bin",
     command => "drush cache-clear drush",
-    subscribe => [Exec["installterminus"], Exec["installterminatur"]],
+    subscribe => Exec["installterminatur"],
   }
 
 }
