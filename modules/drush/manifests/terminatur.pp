163f2eb926e9d1f62d517527e43615c9e52c500c
Mike Pirog <mike@kalamuna.com>
2013-10-24 20:06:22 -0700

    Peg terminatur to alpha2

--- modules/drush/manifests/terminatur.pp
+++ modules/drush/manifests/terminatur.pp
@@ -8,7 +8,7 @@ class drush::terminatur {
   exec { "installterminatur":
     path    => "/bin:/usr/bin",
     unless  => "drush | grep terminatur-aliases",
-    command => "mkdir /usr/share/drush/commands/terminatur && /usr/local/bin/composer create-project kalamuna/terminatur /usr/share/drush/commands/terminatur -s dev --no-dev -n",
+    command => "mkdir /usr/share/drush/commands/terminatur && /usr/local/bin/composer create-project kalamuna/terminatur /usr/share/drush/commands/terminatur 1.0-alpha2 --no-dev -n",
     require => Exec["installcomposer"],
   }
 
