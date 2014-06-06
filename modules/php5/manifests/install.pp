202c1d7fb1a6c317eb6c4c69bd21c73b71546aa1
Emanuel Greucean <emanuel@kalamuna.com>
2014-06-05 22:59:37 -0500

    Add ppa repository for php54

--- modules/php5/manifests/install.pp
+++ modules/php5/manifests/install.pp
@@ -1,12 +1,26 @@
 /**
- * 
+ *
  * Installs PHP5 for CLI
- * 
+ *
  */
 class php5::install {
-	
-	package { "php5-cli":
-		ensure => present,
-	}
-	
+
+  package { "python-software-properties":
+    ensure => present,
+    require => Exec['php54 apt update']
+  }
+
+  exec { "add php54 apt-repo":
+    command => '/usr/bin/add-apt-repository ppa:ondrej/php5-oldstable -y',
+    require => Package['python-software-properties'],
+  }
+
+  exec { "php54 apt update":
+    command => '/usr/bin/apt-get update',
+  }
+
+  package { "php5-cli":
+    ensure => present,
+  }
+
 }
